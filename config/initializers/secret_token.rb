10c7c14dd7a73780b67bd4d96f339844781b14ce
Rohit Kumar <rohitkumar2506@gmail.com>
2013-08-16 13:50:13 +0530

    first cut to start awesomeness.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SaveYourHealth::Application.config.secret_token = '3a6482e894065466b84258e4f21bc061312cf3f4a00f9a10ac353792a9b4a687ac88900b9a08e944ad93249c53bd5b003dab49eb4af17811fc247aa92b9f75aa'
